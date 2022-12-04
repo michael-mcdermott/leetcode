01bae8178561164477534ce0874270471fdcb118
michael-mcdermott <mcdermottm34@gmail.com>
2022-12-04 00:16:04 -0600

    staging files

---
+++ 451. Sort Characters By Frequency/451.py
@@ -0,0 +1,26 @@
+import sys
+import collections
+
+class Solution:
+    
+    def frequencySort(self, s: str) -> str:
+        dict = collections.Counter(s)
+        ans = ''
+        for c in sorted(dict, key=lambda item: dict[item], reverse=True):
+            ans+=c*dict[c]
+        return ans
+
+if __name__== "__main__" :
+
+    # set test cases
+    
+    test1 = ["tree","eetr"]
+    test2 = ["cccaaa", "aaaccc"]
+    test3 = ["Aabb","bbAa"]
+    
+    tests = [test1, test2, test3]
+    # instantiate class
+    tree = Solution
+
+    for i in tests:
+        print(i[1], tree.frequencySort(tree,s=i[0]), i[1] == tree.frequencySort(tree,s=i[0]))
