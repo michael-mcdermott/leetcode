c1dea821d808a10e8e3591bc168b9bc5194a4d6f
michael-mcdermott <mcdermottm34@gmail.com>
2022-12-05 09:53:18 -0600

    revisited problem

--- 2256. Minimum Average Difference/2256.py
+++ 2256. Minimum Average Difference/2256.py
@@ -1,20 +1,24 @@
 class Solution:
     def minimumAverageDifference(self, nums: list[int]) -> int:
-        
-        length = len(nums)
-        print(f'length is {length}')
-        
-        total = 0
-        for i in nums:
-            total += i
-            print(total)
-            leftList = [nums[:i]]
-            rightList = [nums[i:]]
-            print(leftList, rightList)
+        preSum = [nums[0]]
+        for i in range(1, len(nums)):
+            preSum.append(preSum[i-1] + nums[i])
+
+        min_diff = float('inf')
+        min_idx = 0
+
+        for i in range(len(preSum)):
+            diff = abs(preSum[i]//(i+1) - (preSum[-1] - preSum[i])//max(len(preSum)-1 -i,1 ) )
+
+            if diff < min_diff:
+                min_diff = diff
+                min_idx = i
+
+        return min_idx
 
 
 if __name__== "__main__" :
         
-        solution = Solution
-        nums = [2,5,3,9,5,3]
-        print(solution.minimumAverageDifference(solution, nums))
+    solution = Solution
+    nums = [2,5,3,9,5,3]
+    print(solution.minimumAverageDifference(solution, nums))
